30256c288f00b45bd816bfe9bd535ce3bf7e4347
aleksandr_vyskvarko <vyskvarko1992@mail.ru>
2021-09-30 21:59:02 +0300

    init dev branch

--- lib/my_first_page.dart
+++ lib/my_first_page.dart
@@ -27,61 +27,43 @@ class _MyFirstPageState extends State<MyFirstPage> {
         child: Image.asset("assets/images/background.png"),
       );
     }
+
     Widget _addContainer(image, text) {
       return Padding(
-        padding: EdgeInsets.only( top: 13 ),
+        padding: EdgeInsets.only(top: 13),
         child: Align(
           alignment: Alignment.topCenter,
-          child: Container (
+          child: Container(
             width: 343,
             height: 122,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.all(
-                  Radius.circular(20),),
-              boxShadow: [
-                BoxShadow(color: Colors.white, spreadRadius: 0.15),
-              ]
-            ) ,
-            child:
-            Padding(
-              padding:
-                  EdgeInsets.only(top: 21, bottom: 21, left: 21, right: 242),
-              child: image
-
-            ),
-
+                  Radius.circular(20),
+                ),
+                boxShadow: [
+                  BoxShadow(color: Colors.white, spreadRadius: 0.15),
+                ]),
+            child: Padding(
+                padding:
+                    EdgeInsets.only(top: 21, bottom: 21, left: 21, right: 242),
+                child: image),
           ),
         ),
       );
     }
-
 
     return Scaffold(
       body: Stack(
         children: <Widget>[
           _logo(),
           _background(),
-          Column( children: <Widget>[
-            Padding( padding: EdgeInsets.only( top: 207 )),
-          _addContainer(
-           Image.asset("assets/images/face.png"), Text("Мой профиль",
-          )
-          ),
-          _addContainer(
-            Image.asset("assets/images/face2.png"), Text("Мои тренировки",
-          )
-          ),
-
-
-
-  ]
-
-
-
-          ),
-
-
-
+          Column(children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 207)),
+            _addContainer(
+                Image.asset("assets/images/face.png"), Text("Мой профиль")),
+            _addContainer(
+                Image.asset("assets/images/face2.png"), Text("Мои тренировки")),
+          ]),
         ],
       ),
     );
